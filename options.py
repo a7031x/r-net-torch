227ca9a5b4e52cbb12804c7af36fc0172fa0b476
a7031x <a7031x@gmail.com>
2018-07-25 10:44:10 +0800

    .

--- options.py
+++ options.py
@@ -14,7 +14,7 @@ def train_opts(parser):
     group = parser.add_argument_group('train')
     group.add_argument('-batch_size', type=int, default=64)
     group.add_argument('-validate_batch_size', type=int, default=64)
-    group.add_argument('-learning_rate', type=float, default=0.001)
+    group.add_argument('-learning_rate', type=float, default=0.01)
     group.add_argument('-dropout', type=float, default=0.3)
     group.add_argument('-max_grad_norm', type=float, default=5.0)
     group.add_argument('-summary_file', type=str, default='./output/summary.txt')

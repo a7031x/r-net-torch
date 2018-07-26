f503c7bfced54346a05143b564c1a5b0fa69ca7e
a7031x <a7031x@gmail.com>
2018-07-26 08:10:56 +0800

    .

--- optimization.py
+++ optimization.py
@@ -1,12 +1,13 @@
 
 class AutoDecay(object):
-    def __init__(self, optimizer, patience=3, not_learning_counter=5):
+    def __init__(self, optimizer, patience=3, not_learning_counter=5, min_lr=1E-4):
         self.learning_rate = optimizer.defaults['lr']
         self.optimizer = optimizer
         self.counter = 0
         self.patience = patience
         self.not_learning_counter = not_learning_counter
         self.decay_counter= 0
+        self.min_lr = min_lr
 
 
     def better(self):
@@ -18,6 +19,7 @@ class AutoDecay(object):
         self.counter += 1
         if self.counter >= self.patience:
             self.learning_rate /= 2
+            self.learning_rate = max(self.learning_rate, self.min_lr)
             for group in self.optimizer.param_groups:
                 group['lr'] = self.learning_rate
             self.counter = 0
